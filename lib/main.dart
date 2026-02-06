721d12ca496d4583c8efe1ced077b39b57b2f220
CodeShehzadX <137306474+CodeShehzadX@users.noreply.github.com>
2026-02-06 14:51:05 +0500

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:learnings/home_page.dart';
+import 'package:learnings/pages/home_page.dart';
+import 'package:learnings/pages/login_page.dart';
 
 void main() {
   runApp( MyApp());
@@ -12,6 +13,17 @@ double pi =3.142;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      //home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/home": (context)=>HomePage(),
+        "/login": (context)=>LoginPage(),
+      },
     );
   }}
