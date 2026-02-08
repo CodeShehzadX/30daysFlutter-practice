31f8ea88334bc384df414eae8302739d1c0e15da
CodeShehzadX <137306474+CodeShehzadX@users.noreply.github.com>
2026-02-08 21:29:28 +0500

    day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,13 @@
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String imageURL;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.imageURL});
+
+
+
+}
